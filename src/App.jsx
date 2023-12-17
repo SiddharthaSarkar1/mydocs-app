f624e3fe6fc51dee566eea45f2318ac0745198ab
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-12-18 01:43:36 +0530

    basic components creation and setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Background from './components/Background'
+import Foreground from './components/Foreground'
 
 const App = () => {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <div className="absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl">My Documents.</div>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tighter font-semibold text-zinc-900'>My Docs.</h1>
+      <Background />
+      <Foreground />
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="w-60 h-80 bg-zinc-600/40 rounded-[20px]">
+        icon
+        text
+        footer
+        data 
+        tag
+    </div>
+  )
+}
+
+export default Card
